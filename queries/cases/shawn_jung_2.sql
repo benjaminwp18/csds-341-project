7ddf9c0b83ca658bb830a9d77454d291e624ddb8
Shawn Jung <sxj754@case.edu>
2023-05-01 23:03:30 -0400

    Update use case 2 for Shawn

--- queries/cases/shawn_jung_2.sql
+++ queries/cases/shawn_jung_2.sql
@@ -8,30 +8,39 @@ SELECT *
 FROM Storefront
 WHERE Storefront.StoreName LIKE 'Stap%'
 
--- 2. Delete the products owned by the store from the StorefrontsProducts table.
-DELETE FROM StorefrontsProducts
-WHERE StorefrontID = 0;
+-- 2. Create stored procedure 
+CREATE PROCEDURE DeleteClosedStore(@StorefrontID INT)
+AS
+BEGIN
+    BEGIN TRANSACTION;
 
--- 3. Delete the corresponding rows from the CategoriesProducts table
--- if the product was exclusively used in the closed store.
-DELETE FROM CategoriesProducts
-WHERE ProductID IN (
-    SELECT CP.ProductID
-    FROM CategoriesProducts CP
-    LEFT JOIN StorefrontsProducts SP ON CP.ProductID = SP.ProductID
-    WHERE SP.ProductID IS NULL
-);
+    -- 3. Delete the products owned by the store from the StorefrontsProducts table.
+    DELETE FROM StorefrontsProducts
+    WHERE StorefrontID = @StorefrontID;
 
--- 4. Delete the corresponding rows from the WarehousesProducts table
--- if the product was exclusively used in the closed store.
-DELETE FROM WarehousesProducts
-WHERE ProductID IN (
-    SELECT WP.ProductID
-    FROM WarehousesProducts WP
-    LEFT JOIN StorefrontsProducts SP ON WP.ProductID = SP.ProductID
-    WHERE SP.ProductID IS NULL
-);
+    -- 4. Delete the corresponding rows from the CategoriesProducts table
+    -- if the product was exclusively used in the closed store.
+    DELETE FROM CategoriesProducts
+    WHERE ProductID IN (
+        SELECT CP.ProductID
+        FROM CategoriesProducts CP
+        LEFT JOIN StorefrontsProducts SP ON CP.ProductID = SP.ProductID
+        WHERE SP.ProductID IS NULL
+    );
 
--- 5. Delete the closed storefront from the Storefront table.
-DELETE FROM Storefront
-WHERE StorefrontID = 0;
+    -- 5. Delete the corresponding rows from the WarehousesProducts table
+    -- if the product was exclusively used in the closed store.
+    DELETE FROM WarehousesProducts
+    WHERE ProductID IN (
+        SELECT WP.ProductID
+        FROM WarehousesProducts WP
+        LEFT JOIN StorefrontsProducts SP ON WP.ProductID = SP.ProductID
+        WHERE SP.ProductID IS NULL
+    );
+
+    -- 6. Delete the closed storefront from the Storefront table.
+    DELETE FROM Storefront
+    WHERE StorefrontID = @StorefrontID;
+
+    COMMIT;
+END;
